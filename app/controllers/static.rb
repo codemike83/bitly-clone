1963109d3b9b5dd60731d2a166fc54d734866277
MICHAEL JASINGH <michaeljasingh@gmail.com>
2015-11-26 09:51:06 +0800

    final change

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,22 @@
 get '/' do
+	@urls = Url.all
   erb :"static/index"
 end
+
+post '/url' do
+	p params[:user_input_long_url]
+	url = Url.create(long_url: params[:user_input_long_url])
+
+	redirect "/"
+end
+
+get "/:user_input_short_url" do
+	url = Url.find_by(short_url: params[:user_input_short_url])
+
+	unless url.nil?
+		redirect url.long_url
+	else
+		redirect "/"
+	end
+end
+
---
+++ app/models/url.rb
@@ -0,0 +1,12 @@
+class Url < ActiveRecord::Base
+	# This is Sinatra! Remember to create a migration!
+	
+	before_create :shorten
+
+	def shorten
+		self.short_url = SecureRandom.hex(6)
+	end
+
+
+
+end
